473b893eb468c3ff81d5dc10f191bfb61bbc7651
Musa Khan <mmkhan@knox.edu>
2018-03-05 03:03:43 -0600

    Footer change, navbar change, sidenav bar alignment, more to come

--- platforms/browser/www/js/init.js
+++ platforms/browser/www/js/init.js
@@ -1,7 +1,15 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) { /* Do Stuff  */ }, // A function to be called when sideNav is opened
+      onClose: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is closed
+      }
+  );
     $('.parallax').parallax();
 
   }); // end of document ready
